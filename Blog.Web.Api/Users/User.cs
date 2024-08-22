4c8d0f7c9c204b9297454c53193636cc143723d0
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-08-22 17:32:19 +0300

    Feat - adding User & UserDetails entity

---
+++ Blog.Web.Api/Users/User.cs
@@ -0,0 +1,32 @@
+﻿namespace User.Web.Api.Users
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        
+        public required string UserName { get; set; }
+        public string? NormalizedUserName { get; set; }
+
+        public required string Email { get; set; }
+        public required string NormalizedEmail { get; set; }
+        public bool EmailConfirmed { get; set; }
+        
+        public required string PasswordHash { get; set; }
+
+        public string? SecurityStamp { get; set; }
+        public string? ConcurrencyStamp { get; set; }
+
+        public string? PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+
+        public bool TwoFactorEnabled { get; set; }
+
+        public DateTimeOffset? LockoutEnd { get; set; }
+        public bool LockoutEnabled { get; set; }
+
+        public int AccessFailedCount { get; set; }
+
+        public required string Nickname { get; set; }
+        public required string Role { get; set; }
+    }
+}
---
+++ Blog.Web.Api/Users/UserDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace User.Web.Api.Users
+{
+    public class UserDetails
+    {
+        public Guid Id { get; set; }
+        public required string MessagingAccess { get; set; }
+        public DateOnly RegisterDate { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string? PhotoLink { get; set; }
+        public required string UserId { get; set; }
+    }
+}
