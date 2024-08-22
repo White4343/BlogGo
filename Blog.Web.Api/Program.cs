4d57ed88d56d6a255ec97c7341a879ba485bdd3f
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-08-22 16:57:44 +0300

    Feat - init commit

---
+++ Blog.Web.Api/Program.cs
@@ -0,0 +1,16 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(o => o.CustomSchemaIds(id => id.FullName!.Replace('+', '-')));
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello World!");
+
+await app.RunAsync().ConfigureAwait(false);
--- BlogGo/Program.cs
+++ BlogGo/Program.cs
@@ -1,13 +1,22 @@
 namespace BlogGo
 {
+    // TODO: This Error is not meant to be triggered, but with new .editorconfig settings, it is.
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(o => o.CustomSchemaIds(id => id.FullName!.Replace('+', '-')));
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.Run();
         }
